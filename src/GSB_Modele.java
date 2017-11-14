b0e42e288921b49ea80302e10f3b19b1a3347ecf
lolo <moi@gmail.com>
2017-11-14 11:11:00 +0100

    Première sauvegarde
    Je fais une sauvegarde de gsb pour un tp

---
+++ src/GSB_Modele.java
@@ -0,0 +1,64 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+public class GSB_Modele {
+	private static Connection connexion;
+	private static Statement Statement;
+	private static ResultSet rs;
+
+	
+	
+	public static boolean connexion(){
+		boolean rep= false;
+		try{
+			
+			Class.forName("com.mysql.jdbc.Driver") ;
+			connexion = DriverManager.getConnection("jdbc:mysql://localhost/gsbmvc", "root", "");
+			Statement = connexion.createStatement(); 
+			System.out.println("ok");
+		rep = true;
+		}
+		catch  (ClassNotFoundException e) {
+			
+			System.out.println("driveur non chargé");
+		}
+		catch (SQLException e){
+			
+		}
+		
+	return rep;
+	}
+	
+	public static void connection(String unLogin, String unMdp){
+
+	
+	    
+	 
+	    String sql = ("SELECT count(*) login,mdp FROM visiteur WHERE login ='" + unLogin+ "' AND mdp='"+ unMdp+ "';" );
+	   int count= 0;
+		try{
+			
+			rs = Statement.executeQuery(sql);
+			if(rs.next()){
+			count=rs.getInt(1);
+			
+			}
+			if(count!=0){
+			System.out.println("yataaaa");
+			}
+			else{
+				System.out.println("Erreur de connexion Login ou Mots de passe incorecte");
+			}
+			rs.close();
+		}
+		catch(SQLException e){
+			System.out.println("problème sql" + e);
+		}
+	}
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ GSB_Modele.connexion();
+ GSB_Modele.connection("dandre", "oppg5");
+	}
+
+}
